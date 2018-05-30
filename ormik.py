b6eb6ce047e7c790760348fbc57c5bd8e4610d47
Grin941 <labamifi@gmail.com>
2018-05-30 08:03:31 +0300

    Writing Field default values

--- ormik.py
+++ ormik.py
@@ -1,7 +1,14 @@
 class Field:
 
-    def __init__(self, name=None, **kwargs):
+    def __init__(
+        self,
+        name=None, null=False, default=None, primary_key=False,
+        **kwargs
+    ):
         self.name = name
+        self.is_nullable = null
+        self.default_value = default
+        self.is_primary_key = primary_key
 
     def __set__(self, instance, value):
         instance.__dict__[self.name] = value
@@ -9,12 +16,12 @@ class Field:
 
 class SizedField(Field):
 
-    def __init__(self, *args, maxlen=128, **kwargs):
-        self.maxlen = maxlen
+    def __init__(self, *args, max_length=128, **kwargs):
+        self.max_length = max_length
         super().__init__(*args, **kwargs)
 
     def __set__(self, instance, value):
-        if len(value) > self.maxlen:
+        if len(value) > self.max_length:
             raise ValueError(
                 f'"{self.name}" field maxlen should be < {self.maxlen}'
             )
@@ -32,34 +39,27 @@ class TypedField(Field):
         super().__set__(instance, value)
 
 
-class NullableField(Field):
-
-    def __init__(self, *args, nullable=True, **kwargs):
-        self.nullable = True
-        super().__init__(*args, **kwargs)
-
-
-class CharField(TypedField, SizedField, NullableField):
+class CharField(TypedField, SizedField):
     ty = str
 
 
-class IntegerField(TypedField, NullableField):
+class IntegerField(TypedField):
     ty = int
 
 
 class ModelMeta(type):
 
     def __new__(mtcls, name, bases, clsdict):
-        fields_name = [
+        fields_name = {
             key for key, val in clsdict.items() if
             isinstance(val, Field)
-        ]
+        }
 
         for field_name in fields_name:
             clsdict[field_name].name = field_name
 
         model_cls = super().__new__(mtcls, name, bases, clsdict)
-        model_cls._fields = fields_name
+        model_cls._fields = clsdict
 
         return model_cls
 
@@ -67,6 +67,12 @@ class ModelMeta(type):
 class Model(metaclass=ModelMeta):
 
     def __init__(self, *args, **kwargs):
+        for field_name, field in self.__class__._fields.values():
+            field_value = kwargs.get(field_name, None)
+            if field_name in kwargs:
+                setattr(self, field_name, field_value)
+            else:
+
         for field_name, field_value in kwargs.items():
             if field_name in self.__class__._fields:
                 setattr(self, field_name, field_value)
@@ -77,4 +83,4 @@ if __name__ == '__main__':
         a = CharField()
 
     my_model = MyModel(a='123')
-    print(my_model.a)
+    print(my_model.a, my_model.b)
